94c6dcad8ef160f795106497acc22ca13f995458
M.Sackel <maximilian.sackel@gmx.de>
2025-12-28 15:18:27 +0000

    run db

--- app/app.py
+++ app/app.py
@@ -1,6 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, g
+from datetime import datetime 
+import psycopg
 
 app = Flask(__name__)
+
+def get_db():
+    if 'db' not in g:
+        # g.db = psycopg.connect(
+        #         "dbname='testdb' user='testusr' password='testpwd' host='0.0.0.0'"
+        # )
+
+        g.db = psycopg.connect(
+                dbname='testdb',
+                user='testusr', 
+                password='testpwd',
+                host="192.168.178.37"
+        )
+    return g.db
 
 @app.route('/')
 def hello():
@@ -13,6 +29,13 @@ def data():
     temp = data["temp"]
     humi = data["humi"]
     poll = data["poll"]
+    ts = f"{datetime.now()}"
+    
+    get_db().execute(
+        t"INSERT INTO weather (ts, temp, humi, CO2) VALUES ({ts}, {temp}, {humi}, {poll})"
+    )
+
+    get_db().commit()
 
     return f"successfull"
 
